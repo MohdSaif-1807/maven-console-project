096c235b2389f22a7b971431dc60051a3b76d755
MohdSaif-1807 <saifmohd1807@gmail.com>
2022-11-02 10:51:43 +0530

    this is console project

---
+++ src/main/java/in/kmit/thisissamplemavenproject/App.java
@@ -0,0 +1,13 @@
+package in.kmit.thisissamplemavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is the first maven project" );
+    }
+}
